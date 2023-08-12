6e747c83bc44ca704a384b11d74ed79ec18dc2db
Amit Nishad <amitn590@gmail.com>
2023-08-12 16:58:59 +0530

    changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -220,6 +220,52 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="famous-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                  <img src="images/famous-1.jpg" alt="famous"className="img-fluid"/>
+                  <div className="card-content position-absolute">
+                    <h5>Big Screen</h5>
+                    <h6>Smart Watch Series</h6>
+                    <p>From $399 or 16.62/mo. for 24 mo</p>
+                  </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                  <img src="images/famous-1.jpg" alt="famous"className="img-fluid"/>
+                  <div className="card-content position-absolute">
+                    <h5>Big Screen</h5>
+                    <h6>Smart Watch Series</h6>
+                    <p>From $399 or 16.62/mo. for 24 mo</p>
+                  </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                  <img src="images/famous-1.jpg" alt="famous"className="img-fluid"/>
+                  <div className="card-content position-absolute">
+                    <h5>Big Screen</h5>
+                    <h6>Smart Watch Series</h6>
+                    <p>From $399 or 16.62/mo. for 24 mo</p>
+                  </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                  <img src="images/famous-1.jpg" alt="famous"className="img-fluid"/>
+                  <div className="card-content position-absolute">
+                    <h5>Big Screen</h5>
+                    <h6>Smart Watch Series</h6>
+                    <p>From $399 or 16.62/mo. for 24 mo</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="special-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
@@ -232,6 +278,19 @@ const Home = () => {
               <SpecialProducts />
               <SpecialProducts />
               <SpecialProducts />
+          </div>
+        </div>
+      </section>
+      <section className="popular-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Our Popular Products</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
           </div>
         </div>
       </section>
